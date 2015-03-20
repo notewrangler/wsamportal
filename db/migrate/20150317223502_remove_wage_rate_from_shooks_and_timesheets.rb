0f8b2ece4ec0e61bc23486a34881d2c036d7e4ee
Ralph Johnson <noiseartist@gmail.com>
2015-03-20 11:33:01 -0700

    removed migration remove wage_rate

--- db/migrate/20150317223502_remove_wage_rate_from_shooks_and_timesheets.rb
+++
@@ -1,6 +0,0 @@
-class RemoveWageRateFromShooksAndTimesheets < ActiveRecord::Migration
-  def change
-  	remove_column :shooks, :wage_rate, :decimal
-  	remove_column :timesheets, :wage_rate, :decimal
-  end
-end

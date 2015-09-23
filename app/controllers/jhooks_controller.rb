2150440c1aa1abcc6ebefa0051e2d12a22b8548a
ralph <noiseartist@gmail.com>
2015-09-23 15:47:37 -0700

    minor changes & jQuery reveal on login page

--- app/controllers/jhooks_controller.rb
+++ app/controllers/jhooks_controller.rb
@@ -5,36 +5,36 @@ class JhooksController < ApplicationController
 	 		@jhook = Jhook.new
 	    @job = Job.find(params[:job_id])
 	    @shifts_by_date = @job.shifts.all.group_by(&:shift_date)
-	    @date = params[:date] ? Date.parse(params[:date]) : @job.start_date 
+	    @date = params[:date] ? Date.parse(params[:date]) : @job.start_date
 	 end
 
 	 def available_agents
 	 	@job = Job.find(params[:job_id])
 	 	@jhooks = @job.jhooks.all
 	 end
-		
-	
+
+
 		def create
-			@job = Job.find_by(params[:job_id])	
-			@jhook = Jhook.new(jhook_params)		
-										
+			@job = Job.find_by(params[:job_id])
+			@jhook = Jhook.new(jhook_params)
+
 			state = @jhook.state
 			case state
-			  when "all_in" 				  	
-			  	@jhook.update_attributes(agent_id: current_agent.id)							  	
+			  when "all_in"
+			  	@jhook.update_attributes(agent_id: current_agent.id)
 	  			@jhook.job.shifts.each { |shift| shift.increment!(:available_agents) }
-	  			splay_opted(@jhook)	
-	  			flash[:success] = 'Availability Successfully Updated'			  	
-			  	redirect_to jobs_path 
-			  when "unavailable"				  	
+	  			splay_opted(@jhook)
+	  			flash[:success] = 'Availability Successfully Updated'
+			  	redirect_to jobs_path
+			  when "unavailable"
 			  	@jhook.update_attributes(agent_id: current_agent.id)
 			  	flash[:success] = 'Availability Successfully Updated'
 			  	redirect_to jobs_path
-			  when "selected_shifts"			  	
-			  	@jhook.update_attributes(agent_id: current_agent.id)	
-			  	splay_open(@jhook)			  				  	
-			  	redirect_to	shift_calendar_job_jhooks_path(@jhook.job_id)	
-			end 		  	
+			  when "selected_shifts"
+			  	@jhook.update_attributes(agent_id: current_agent.id)
+			  	splay_open(@jhook)
+			  	redirect_to	shift_calendar_job_jhooks_path(@jhook.job_id)
+			end
 	 end
 
 	  def assign_team_lead
@@ -48,25 +48,25 @@ class JhooksController < ApplicationController
 	  	@agent = Agent.find(params[:agent_id])
 		  	@jhook = Jhook.find_or_create_by(:agent_id => @agent.id, :job_id => @job.id)
 		  	@jhook.update_attributes(team_lead: true, state: 'assigned')
-	  		splay_assigned(@jhook)				
-				redirect_to wage_setter_job_jhooks_path(@job)	  	
-		end	  	
-	  	
-	  
-	  def assign	  	
+	  		splay_assigned(@jhook)
+				redirect_to wage_setter_job_jhooks_path(@job)
+		end
+
+
+	  def assign
 	  	@job = Job.find(params[:job_id])
 		  @jhooks = Jhook.find(params[:ids])
 
 		  if @jhooks.length <= @job.manpower
-				  @jhooks.each do |jhook| 
-				  	jhook.update_attributes(state: 'assigned')		    
-				  	splay_assigned(jhook)				
-					end				
-				  	redirect_to wage_setter_job_jhooks_path(@job)	
+				  @jhooks.each do |jhook|
+				  	jhook.update_attributes(state: 'assigned')
+				  	splay_assigned(jhook)
+					end
+				  	redirect_to wage_setter_job_jhooks_path(@job)
 			else
 			 	flash[:danger] = 'Number of Assigned Agents exceeds Required Agents'
 				redirect_to available_agents_job_jhooks_path
-			end	
+			end
 
 	  end
 
@@ -75,22 +75,20 @@ class JhooksController < ApplicationController
 	 	@jhooks = @job.jhooks.all
 	 end
 
-	 def unassign	  	
+	 def unassign
 	  	@job = Job.find(params[:job_id])
 		  @jhooks = Jhook.find(params[:ids])
-		  @jhooks.each do |jhook| 
-		  	jhook.update_attributes(state: 'removed')		    
-		  	splay_removed(jhook)				
-			end	
+		  @jhooks.each do |jhook|
+		  	jhook.update_attributes(state: 'removed')
+		  	splay_removed(jhook)
+			end
 				flash[:success] = 'Agent was Successfully Removed.'
-		  	redirect_to jobs_path 				  					 	
+		  	redirect_to jobs_path
 	  end
-
-
 
 	  def wage_setter
 	  	@job = Job.find(params[:job_id])
-	  	@jhooks = @job.jhooks.assigned.nopay	  	
+	  	@jhooks = @job.jhooks.assigned.nopay
 	  end
 
 	  def wage_setter_partial
@@ -107,7 +105,7 @@ class JhooksController < ApplicationController
 	  def shift_calendar
 	  	@job = Job.find(params[:job_id])
 	   	@shifts_by_date = @job.shifts.all.group_by(&:shift_date)
-	    @date = params[:date] ? Date.parse(params[:date]) : @job.start_date 
+	    @date = params[:date] ? Date.parse(params[:date]) : @job.start_date
 	  end
 
 	  def shift_selections
@@ -116,9 +114,9 @@ class JhooksController < ApplicationController
 	  		@shifts.each do |shift|
 		  		@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => current_agent.id)
 	  			@shook.opt!
-	  		end	
-	  		flash[:success] = 'Shifts were Successfully Selected'	 
-		  	redirect_to jobs_path 
+	  		end
+	  		flash[:success] = 'Shifts were Successfully Selected'
+		  	redirect_to jobs_path
 		 end
 
 		def assign_manual
@@ -126,43 +124,43 @@ class JhooksController < ApplicationController
 			@agent_select = Agent.all.order(:last_name).map{|a| [ a.alpha_handle + ": " + a.username,  a.id] }
 		end
 
-		def manual_assignment			
+		def manual_assignment
 			@job = Job.find(params[:job_id])
-    	@agent = Agent.find(params[:agent_id]) 
+    	@agent = Agent.find(params[:agent_id])
      	@jhook =	Jhook.find_or_create_by(agent_id: @agent.id, job_id: @job.id)
      	@jhook.assign!
-     	splay_assigned(@jhook)		 		   	 
-      redirect_to wage_setter_job_jhooks_path(@job) 
-		end 
-		
+     	splay_assigned(@jhook)
+      redirect_to wage_setter_job_jhooks_path(@job)
+		end
+
 		def destroy
 		end
 
 		private
 
-		def splay_opted(jhook)			
+		def splay_opted(jhook)
 			@jhook = jhook
 			@shifts = @jhook.job.shifts.all
 			@shifts.each do |shift|
 				@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => @jhook.agent_id)
 				@shook.update_attributes(jhook_id: @jhook.id, state: 'opted', date: shift.shift_date)
 			end
-		end	
+		end
 
 		def splay_open(jhook)
 			@jhook = jhook
 			@shifts = @jhook.job.shifts.all
-			@shifts.each do |shift| 
-				@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => @jhook.agent.id) 
+			@shifts.each do |shift|
+				@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => @jhook.agent.id)
 			  @shook.update_attributes(jhook_id: @jhook.id, state: 'open', date: shift.shift_date)
 			end
-		end	
+		end
 
 		def splay_assigned(jhook)
 			@jhook = jhook
 			@shifts = @jhook.job.shifts.all
 			@shifts.each do |shift|
-				@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => @jhook.agent.id) 
+				@shook = Shook.find_or_create_by(:shift_id => shift.id, :agent_id => @jhook.agent.id)
 				@shook.update_attributes(jhook_id: @jhook.id, state: 'assigned', date: shift.shift_date)
 			end
 		end
@@ -171,12 +169,12 @@ class JhooksController < ApplicationController
 			@jhook = jhook
 			@shifts = @jhook.job.shifts.all
 			@shifts.each do |shift|
-				@shook = Shook.find_by(:shift_id => shift.id, :agent_id => @jhook.agent.id) 
+				@shook = Shook.find_by(:shift_id => shift.id, :agent_id => @jhook.agent.id)
 				@shook.update_attributes(jhook_id: @jhook.id, state: 'removed', date: shift.shift_date)
 			end
 		end
-			
-		
+
+
 		def set_jhook
 			@jhook = Jhook.find(params[:id])
 		end
@@ -186,7 +184,3 @@ class JhooksController < ApplicationController
 	  end
 
 end
-
-
-
-

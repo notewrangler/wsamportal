d2c1bf8e2facbdb9c8d2613024b2e46372c364b1
ralph <noiseartist@gmail.com>
2016-01-23 21:33:19 -0800

    added signin bar to agent home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def index
-    @today = Shook.assigned.select(['agent_id = ? and date = ?', current_agent.id,  Date.today]).pluck(:id)
-    
+
   end
 
-  
+
 
   def create
   	user = User.find_by(username: params[:session][:username])
@@ -27,5 +26,15 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 
-  
+
 end
+
+# if current_user.role == 'agent'
+#   hot = Shook.try(:assigned)
+#   ourHero = hot.try(current_agent.id)
+#   today = ourHero.try(date: Date.today)
+#   if today
+#     @today = today.pluck(:id)
+#   end
+#   # @today = Shook.assigned.select.(['agent_id = ? and date = ?', current_agent.id,  Date.today]).pluck(:id) || false
+# end
